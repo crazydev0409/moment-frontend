168fc2651845cb3274aeb218d4234564a1961225
Crazy Dev <crazydev0409@gmail.com>
2026-01-19 11:32:15 -0600

    fix: Adjust margin top in SignupScreen's TextInput for improved layout consistency

--- src/navigation/AuthStack/AuthStack_SignupScreen.tsx
+++ src/navigation/AuthStack/AuthStack_SignupScreen.tsx
@@ -142,7 +142,7 @@ const AuthStack_SignupScreen: React.FC<Props> = ({ navigation, route }) => {
               +{callingCode}
             </Text>
             <TextInput
-              style={[tw`bg-white rounded-lg flex-1 font-dm font-bold`, { fontSize: moderateScale(16.875), marginTop: verticalScale(2.625) }]}
+              style={[tw`bg-white rounded-lg flex-1 font-dm font-bold`, { fontSize: moderateScale(16.875), marginTop: verticalScale(0.5) }]}
               value={phone}
               placeholder="Phone Number?"
               onChangeText={setPhone}
